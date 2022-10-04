e50999c2529bcf513e165155dcc438f435af2ea6
IkariPandora <98650407+IkariPandora@users.noreply.github.com>
2022-10-04 10:59:52 +0600

    Initial commit

---
+++ src/main/java/com/beaiteam/project1beai/Project1BeaiApplication.java
@@ -0,0 +1,13 @@
+package com.beaiteam.project1beai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1BeaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1BeaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beaiteam/project1beai/Project1BeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaiteam.project1beai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1BeaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
